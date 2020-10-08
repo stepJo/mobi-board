46a4a53c2895638abfd5e62af2bf9711e74205da
stepJo <ephenjo99@gmail.com>
2020-10-08 19:05:38 +0700

    Add vue toasted package

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,41 @@ import router from "./router";
 import { ValidationProvider } from "vee-validate/dist/vee-validate.full.esm";
 import { ValidationObserver } from "vee-validate";
 import VModal from "vue-js-modal";
+import Toasted from "vue-toasted";
+import Vue from "vue";
 
 window.Vue = require("vue");
 
 Vue.use(VueRouter);
 Vue.use(VModal);
+Vue.use(Toasted, {
+    iconPack: "fontawesome"
+});
+Vue.toasted.register(
+    "successNotification",
+    payload => {
+        if (!payload.message) {
+            return "Success";
+        }
+        return payload.message;
+    },
+    {
+        icon: {
+            name: "check-circle"
+        },
+        after: true,
+        keepOnHover: true,
+        duration: 2000,
+        position: "top-right",
+        type: "success",
+        action: {
+            text: "CLOSE",
+            onClick: (e, toastObject) => {
+                toastObject.goAway(0);
+            }
+        }
+    }
+);
 
 Vue.component("ValidationProvider", ValidationProvider);
 Vue.component("ValidationObserver", ValidationObserver);
